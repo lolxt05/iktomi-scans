79b05b09c31920dcd8322683fe39cd3330a9b490
Nikolai Fessl <nikolai.fessl@samariterbund.net>
2025-01-29 15:52:21 +0100

    idk ui docker file beginnings

--- bin/controller.py
+++ bin/controller.py
@@ -1,9 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)    #For debugging
+#make a basic controller ui and also handle all the sceduling here
